8aa15e583a7634c9cacd273085bd2eb00d52805c
Zana <zana@zewin.com>
2026-09-17 17:34:14 +0300

    add app icon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,11 +29,15 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes {
+        buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
